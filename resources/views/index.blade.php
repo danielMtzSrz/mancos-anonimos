c97520532c3894696f0781453fe50f6a695a113f
Daniel Martínez Suárez <danielmartinezisc98@outlook.com>
2022-02-28 11:17:02 -0600

    Creación del proyecto de Mancos Anónimos

---
+++ resources/views/index.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.web')
+
+@section('titulo_apartado')
+    Menú principal
+@endsection
+
+@section('content')
+    <h1 class="animate__animated animate__zoomInDown">Hola</h1>
+@endsection
---
+++ resources/views/layouts/web.blade.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Curso de Programación Web</title>
+    <!-- Archivos para ejecutar tailwind localmente -->
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
+    <script src="{{ asset('js/app.js') }}"></script>
+    <!-- Fin de archivos para ejecutar tailwind localmente  -->
+    <!-- <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">-->
+</head>
+<body>
+    <header class="shadow-lg">
+        <x-app-layout>
+            <x-slot name="header">
+                <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                    @yield('titulo_apartado')
+                </h2>
+            </x-slot>
+    
+            <div class="py-12">
+                <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                    <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+                        
+                    </div>
+                </div>
+            </div>
+        </x-app-layout>
+    </header>
+    <main class="py-10">
+        <div class="container mx-auto px-4">
+            @yield('content')
+        </div>
+    </main>
+    <footer class="py-4 text-center">
+        @auth
+            <a class="text-sm text-gray-700 underline" href="{{ url('dashboard') }}">Dashboard</a>
+        @else
+            <a class="text-sm text-gray-700 underline" href="{{ url('login') }}">Login</a>
+            <a class="text-sm text-gray-700 underline" href="{{ url('register') }}">Registro</a>
+        @endauth
+    </footer>
+</body>
+</html>
